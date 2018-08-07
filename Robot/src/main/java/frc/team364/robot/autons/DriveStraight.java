7503605331e0ef7e263b37051eb3b34b5ac82993
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-08-07 03:09:13 -0500

    Comments
    Added description system for autos

--- Robot/src/main/java/frc/team364/robot/autons/DriveStraight.java
+++ Robot/src/main/java/frc/team364/robot/autons/DriveStraight.java
@@ -3,11 +3,16 @@ package frc.team364.robot.autons;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team364.robot.commands.auto.drive.*;
 
+/**
+ * Auto file - Objective - Cross the line
+ */
 public class DriveStraight extends CommandGroup {
-
+    /**
+     *<p>1 Drive Foward
+     */
     public DriveStraight() {
        
-        addSequential(new DriveStraightForCounts(15000, false, true)); // Drive past black line
+        addSequential(new DriveStraightForCounts(15000, false, true)); //1
     
     }
 }
