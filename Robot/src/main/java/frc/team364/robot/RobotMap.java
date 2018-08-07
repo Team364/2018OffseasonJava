9d8350be4ac6d214d98075c829609f5fa2eba73a
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-08-07 01:55:43 -0500

    Comments RobotMap
    Added descriptoin comments to objects in robot map with designation for which subsytem the object is intended for

--- Robot/src/main/java/frc/team364/robot/RobotMap.java
+++ Robot/src/main/java/frc/team364/robot/RobotMap.java
@@ -3,22 +3,75 @@ package frc.team364.robot;
 public class RobotMap {
 
     // Drive objects
+    /**
+     * leftFrontDrive 
+     * is the motor front motor on the left trasmission - drive system
+     */
     public static final int leftFrontDrive = 16;
+
+    /**
+     * leftRearDrive
+     * is the motor in the rear of the left transmission - drive system
+     */
     public static final int leftRearDrive = 12;
+
+    /**
+     * rightFrontDrive
+     * is the motor in the front of the right transmission - drive system
+     */
     public static final int rightFrontDrive = 9;
+
+    /**
+     * rightRearDrive
+     * is the motor in the rear of the right transmission - drive system
+     */
     public static final int rightRearDrive = 2;
+
+    /**
+     * shifterPort1
+     * is the shifter port corresponding to forward for the drive train shifters - drive system
+     */
     public static final int shifterPort1 = 3;
+
+    /**
+     * shifterPort2
+     * is the shifter port corresponding to reverse for the drive train shifters - drive system
+     */
     public static final int shifterPort2 = 4;
 
-    // First stage elevator objects
+
+    //Arm Objects
+    /**
+     * arm
+     * is the motor for the arm - arm system
+     */
     public static final int arm = 6;
 
     // Intake objects
+    /**
+     * intakeLeft
+     * is the intake motor on the left side of the intakeSystem on the claw -the intake system
+     */
     public static final int intakeLeft = 4;
+
+    /**
+     * intakeRight
+     * is the intake motor on the right side of the intakeSystem on the claw - the intake system
+     */
     public static final int intakeRight = 5;
 
+
     //Pinch objects
+    /**
+     * pinchPistonPort1
+     * is the pison port corresponding to forward for the pincher -- claw system
+     */
     public static final int pinchPistonPort1 = 2;
+
+    /**
+     * pinchPistonPort2
+     * is the piston port corresponding to reverse for the pincher -- claw system
+     */
     public static final int pinchPistonPort2 = 5;
 
 }
