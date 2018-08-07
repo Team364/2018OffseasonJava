3f371e236932fedf6e5194c6135e80107936e9e6
Keanu Clark <35940194+da1andonly68@users.noreply.github.com>
2018-08-07 01:25:47 -0500

    Comments
    Finished adding in the @param for the DriveStraightForCounts method

--- Robot/src/main/java/frc/team364/robot/subsystems/DriveSystem.java
+++ Robot/src/main/java/frc/team364/robot/subsystems/DriveSystem.java
@@ -123,6 +123,8 @@ public class DriveSystem extends Subsystem {
      * driveStraightToEcnoderCounts()
      * Uses the TalonSRX PID to drive to a certain number of counts
      * @param counts specify encoder counts to drive to
+     * @param backwards indicate whether the robot is to move backwards
+     * @param useGyro indicate whether the robot is to use gyro to correct path
      */ 
     public void driveStraightToEncoderCounts(int counts, boolean backwards, boolean useGyro) {
         if(backwards) {
